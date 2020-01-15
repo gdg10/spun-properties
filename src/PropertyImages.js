a18c724b2d8d30085983242a69a05310349e8080
Garrett Grube <ggrube@quartethealth.com>
2020-01-15 14:35:03 -0500

    update pic

--- src/PropertyImages.js
+++ src/PropertyImages.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 import styled from "styled-components";
-import h2 from "src/bushkill/h2.jpeg";
-import h3 from "src/bushkill/h3.jpeg";
-import h4 from "src/bushkill/h4.jpeg";
-import h5 from "src/bushkill/h5.jpeg";
-import h6 from "src/bushkill/h6.jpeg";
-import h7 from "src/bushkill/h7.jpeg";
-import h8 from "src/bushkill/h8.jpeg";
-import h9 from "src/bushkill/h9.jpeg";
-import h10 from "src/bushkill/h10.jpeg";
-import h11 from "src/bushkill/h11.jpeg";
-import h12 from "src/bushkill/h12.jpeg";
+import h0 from "./bushkill/house1.jpeg";
+import h1 from "./bushkill/house2.JPG";
+import h2 from "./bushkill/h2.jpeg";
+import h3 from "./bushkill/h3.jpeg";
+import h4 from "./bushkill/h4.jpeg";
+import h5 from "./bushkill/h5.jpeg";
+import h6 from "./bushkill/h6.jpeg";
+import h7 from "./bushkill/h7.jpeg";
+import h8 from "./bushkill/h8.jpeg";
+import h9 from "./bushkill/h9.jpeg";
+import h10 from "./bushkill/h10.jpeg";
+import h11 from "./bushkill/h11.jpeg";
+import h12 from "./bushkill/h12.jpeg";
 
-const picArray = { h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12 };
+const picArray = [ h0, h1, h3, h4, h12, h5, h6, h8, h9, h10,  h11, h2, h7 ];
 
 const Wrapper = styled.div`
   margin-bottom: 10px;
